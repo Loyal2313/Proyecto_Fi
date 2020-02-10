3a29b3bec17666be4b3513bb73f1f7314b169273
Matt S <loyal2313@gmail.com>
2020-02-10 08:40:53 +0100

    Se añade el script del enemigo para las persecuciones.

--- PacMan_Final/Assets/Scripts/Enemigo.cs
+++ PacMan_Final/Assets/Scripts/Enemigo.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemigo : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    //Velocidad
+    public float velocidad = 20.0f;
+
+    GameObject jugador;
+
+    Vector2 posicion;
+
     void Start()
     {
-        
+
+        jugador = GameObject.FindGameObjectWithTag("Jugador");
+
+        posicion = transform.position;
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        Vector2 presa = posicion;
+
+        float distancia = Vector2.Distance(jugador.transform.position, transform.position);
+        presa = jugador.transform.position;
+
+        float movimiento = velocidad * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, presa, movimiento);
     }
 }
