2cd71cc1003327ed2e18958f4463a5b5e0931487
Matt S <loyal2313@gmail.com>
2020-02-10 06:57:37 +0100

    Movimiento del jugador

---
+++ PacMan_Final/Assets/Scripts/Jugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+
+	//Velocidad
+	public float velocidad = 30.0f;
+
+
+	// Es llamado una vez cada fixed frame
+	void FixedUpdate()
+	{
+
+		//Capto el valor del eje vertical y horizontal de la raqueta
+		float v = Input.GetAxisRaw("Vertical");
+		float h = Input.GetAxisRaw("Horizontal");
+		//Modifico la velocidad de la raqueta
+		GetComponent<Rigidbody2D>().velocity = new Vector2(h * velocidad, v * velocidad);
+
+	}
+}
