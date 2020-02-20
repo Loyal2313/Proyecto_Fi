2cefaa6cd1624e1d6799627007d6cc6089c59ba7
Hugo Hill <57966292+AlphaAnimal@users.noreply.github.com>
2020-02-20 02:31:54 +0100

    Update index.php

--- Unity/view/admin/index.php
+++ Unity/view/admin/index.php
@@ -1,11 +1,5 @@
 <?php
 
-function ImprimeArray($array){
-    echo "<pre>";
-    print_r($array);
-    echo "</pre>";
-}
-
 echo "<h3>Usuarios Activos</h3>";
 
 $db = new mysqli('localhost', 'root', 'Uhhyeah123*', 'unity');
@@ -27,9 +21,9 @@ if (isset($_POST['activar'])){
 $resultado = $db-> query('SELECT * FROM usuarios WHERE activo = 1 AND admin = 0');
 $usuario = $resultado->fetch_array(MYSQLI_BOTH);
 while ($usuario != null){ //Recorro el resultado
-    echo "<p><form action='' style='border: solid 2px black; padding: 3%' method='POST'><span style='font-weight: bold'>Usuario:</span> "
+    echo "<p style='text-align: left'><form action='' style='border: solid 2px black; padding: 3%' method='POST'><span style='font-weight: bold'>Usuario:</span> "
         .$usuario[1]." <input name='borrar' readonly value='".$usuario[0]."'><span style='font-weight: bold'>Última conexión: </span>"
-        .$usuario[3]."<br><br><button type='submit' style='margin-left: 5%;'>Borrar Usuario</button></form></p>";
+        .$usuario[3]."<br><br><button type='submit' style='margin-left: 5%; background-color: red'>Borrar Usuario</button></form></p>";
     $usuario = $resultado->fetch_array(MYSQLI_BOTH);
 }
 $resultado->free(); //Libero de la memoria
@@ -43,9 +37,9 @@ echo "<h3>Usuarios Inactivos</h3>";
 $resultado = $db-> query('SELECT * FROM usuarios WHERE activo = 0 AND admin = 0');
 $usuario = $resultado->fetch_array(MYSQLI_BOTH);
 while ($usuario != null){ //Recorro el resultado
-    echo "<p><form action='' style='border: solid 2px black; padding: 3%' method='POST'><span style='font-weight: bold'>Usuario:</span> "
+    echo "<p class='usuario'><form action='' style='border: solid 2px black; padding: 3%' method='POST'><span style='font-weight: bold'>Usuario:</span> "
         .$usuario[1]." <input name='activar' readonly value='".$usuario[0]."'><span style='font-weight: bold'>Última conexión: </span>"
-        .$usuario[3]."<br><br><button type='submit' style='margin-left: 5%;'>Activar Usuario</button></form></p>";
+        .$usuario[3]."<br><br><button type='submit' style='margin-left: 5%; background-color: green'>Activar Usuario</button></form></p>";
     $usuario = $resultado->fetch_array(MYSQLI_BOTH);
 }
 $resultado->free(); //Libero de la memoria
