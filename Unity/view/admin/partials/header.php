61093c49c7c2fb56228e1ab01fafd3ba2cb5ee88
Hugo Hill <57966292+AlphaAnimal@users.noreply.github.com>
2020-02-20 02:30:48 +0100

    Update header.php

--- Unity/view/admin/partials/header.php
+++ Unity/view/admin/partials/header.php
@@ -10,6 +10,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" type="text/css" href="<?php echo $_SESSION['public'] ?>css/admin.css">
 
+    <!--FUENTES-->
+    <link href="https://fonts.googleapis.com/css?family=Tomorrow&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Exo+2&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
@@ -53,13 +58,14 @@
 <main>
 
     <header>
-        <h1>Administración de Usuarios</h1>
+        <h1>¡¡¡Zona Admin!!!</h1>
 
         <?php if (isset($_SESSION['usuario'])){ ?>
 
             <h2>
                 Usuario: <strong><?php echo $_SESSION['usuario'] ?></strong>
             </h2>
+            <h6 style="text-align: center; font-style: italic">Realizar acciones con precaución, ya que tendrán impacto permanente sobre la base de datos</h6>
 
         <?php } ?>
 
